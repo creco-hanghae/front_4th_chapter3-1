61c48e20667d55064b9505c706678027037d6bde
divopsor <divopsor@gmail.com>
2025-02-07 02:29:23 +0900

    test: medium 훅 테스트 작성

--- src/__tests__/hooks/medium.useEventOperations.spec.ts
+++ src/__tests__/hooks/medium.useEventOperations.spec.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 import { http, HttpResponse } from 'msw';
 
 import {
@@ -6,6 +6,7 @@ import {
   setupMockHandlerDeletion,
   setupMockHandlerUpdating,
 } from '../../__mocks__/handlersUtils.ts';
+import { events } from '../../__mocks__/response/realEvents.json';
 import { useEventOperations } from '../../hooks/useEventOperations.ts';
 import { server } from '../../setupTests.ts';
 import { Event } from '../../types.ts';
@@ -21,16 +22,255 @@ vi.mock('@chakra-ui/react', async () => {
   };
 });
 
-it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {});
+it('저장되어있는 초기 이벤트 데이터를 적절하게 불러온다', async () => {
+  setupMockHandlerCreation(events as Event[]);
 
-it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {});
+  const { result } = renderHook(() => useEventOperations(false));
 
-it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {});
+  await waitFor(() => expect(result.current.events.length).not.toBe(0));
 
-it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {});
+  expect(result.current.events).toEqual(events);
+});
 
-it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {});
+it('정의된 이벤트 정보를 기준으로 적절하게 저장이 된다', async () => {
+  const newEvent = { title: '새로운 이벤트', endTime: '2024-10-03T10:00:00Z' };
+  setupMockHandlerCreation([newEvent] as Event[]);
 
-it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {});
+  const { result } = renderHook(() => useEventOperations(false));
 
-it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {});
+  await act(async () => {
+    await result.current.saveEvent(newEvent as Event);
+  });
+
+  expect(result.current.events).toMatchInlineSnapshot(`
+    [
+      {
+        "endTime": "2024-10-03T10:00:00Z",
+        "title": "새로운 이벤트",
+      },
+      {
+        "endTime": "2024-10-03T10:00:00Z",
+        "id": "2",
+        "title": "새로운 이벤트",
+      },
+    ]
+  `);
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 1000,
+    status: 'info',
+    title: '일정 로딩 완료!',
+  });
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 3000,
+    status: 'success',
+    isClosable: true,
+    title: '일정이 추가되었습니다.',
+  });
+});
+
+it("새로 정의된 'title', 'endTime' 기준으로 적절하게 일정이 업데이트 된다", async () => {
+  const updatedEvent = {
+    id: 1,
+    title: '업데이트된 이벤트',
+    endTime: '2024-10-01T12:00:00Z',
+  };
+
+  setupMockHandlerUpdating();
+
+  const { result } = renderHook(() => useEventOperations(true));
+  await act(async () => {
+    await result.current.saveEvent(updatedEvent as unknown as Event);
+  });
+
+  expect(result.current.events).toMatchInlineSnapshot(`
+    [
+      {
+        "category": "업무",
+        "date": "2024-10-15",
+        "description": "기존 팀 미팅",
+        "endTime": "2024-10-01T12:00:00Z",
+        "id": 1,
+        "location": "회의실 B",
+        "notificationTime": 10,
+        "repeat": {
+          "interval": 0,
+          "type": "none",
+        },
+        "startTime": "09:00",
+        "title": "업데이트된 이벤트",
+      },
+      {
+        "category": "업무 회의",
+        "date": "2024-10-15",
+        "description": "기존 팀 미팅 2",
+        "endTime": "12:00",
+        "id": "2",
+        "location": "회의실 C",
+        "notificationTime": 5,
+        "repeat": {
+          "interval": 0,
+          "type": "none",
+        },
+        "startTime": "11:00",
+        "title": "기존 회의2",
+      },
+    ]
+  `);
+});
+
+it('존재하는 이벤트 삭제 시 에러없이 아이템이 삭제된다.', async () => {
+  const eventIdToDelete = '1';
+  setupMockHandlerDeletion();
+
+  const { result } = renderHook(() => useEventOperations(true));
+
+  await waitFor(() => expect(result.current.events.length).not.toBe(0));
+
+  expect(result.current.events).toMatchInlineSnapshot(`
+    [
+      {
+        "category": "기타",
+        "date": "2024-10-15",
+        "description": "삭제할 이벤트입니다",
+        "endTime": "10:00",
+        "id": "1",
+        "location": "어딘가",
+        "notificationTime": 10,
+        "repeat": {
+          "interval": 0,
+          "type": "none",
+        },
+        "startTime": "09:00",
+        "title": "삭제할 이벤트",
+      },
+    ]
+  `);
+
+  await act(async () => {
+    await result.current.deleteEvent(eventIdToDelete);
+  });
+
+  expect(result.current.events).toMatchInlineSnapshot(`[]`);
+});
+
+it("이벤트 로딩 실패 시 '이벤트 로딩 실패'라는 텍스트와 함께 에러 토스트가 표시되어야 한다", async () => {
+  toastFn.mockReset();
+  server.use(
+    http.get('/api/events', () => {
+      return HttpResponse.json({ message: '오류가 발생했습니다.' }, { status: 500 });
+    })
+  );
+
+  renderHook(() => useEventOperations(false));
+
+  await new Promise((r) => setTimeout(r, 100));
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 3000,
+    isClosable: true,
+    status: 'error',
+    title: '이벤트 로딩 실패',
+  });
+});
+
+it("존재하지 않는 이벤트 수정 시 '일정 저장 실패'라는 토스트가 노출되며 에러 처리가 되어야 한다", async () => {
+  setupMockHandlerUpdating();
+
+  const { result } = renderHook(() => useEventOperations(true));
+
+  await waitFor(() => expect(result.current.events.length).not.toBe(0));
+
+  await act(async () => {
+    await result.current.saveEvent({
+      id: '0',
+      title: '바뀐 회의',
+    } as Event);
+  });
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 1000,
+    status: 'info',
+    title: '일정 로딩 완료!',
+  });
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 3000,
+    isClosable: true,
+    status: 'error',
+    title: '일정 저장 실패',
+  });
+});
+
+it("네트워크 오류 시 '일정 삭제 실패'라는 텍스트가 노출되며 이벤트 삭제가 실패해야 한다", async () => {
+  toastFn.mockReset();
+  setupMockHandlerDeletion();
+  server.use(
+    http.delete('/api/events/:id', () => {
+      return HttpResponse.json({ message: '오류가 발생했습니다.' }, { status: 500 });
+    })
+  );
+
+  const eventIdToDelete = '1';
+  const { result } = renderHook(() => useEventOperations(true));
+
+  await waitFor(() => expect(result.current.events.length).not.toBe(0));
+
+  expect(result.current.events).toMatchInlineSnapshot(`
+    [
+      {
+        "category": "기타",
+        "date": "2024-10-15",
+        "description": "삭제할 이벤트입니다",
+        "endTime": "10:00",
+        "id": "1",
+        "location": "어딘가",
+        "notificationTime": 10,
+        "repeat": {
+          "interval": 0,
+          "type": "none",
+        },
+        "startTime": "09:00",
+        "title": "삭제할 이벤트",
+      },
+    ]
+  `);
+
+  await act(async () => {
+    await result.current.deleteEvent(eventIdToDelete);
+  });
+
+  expect(result.current.events).toMatchInlineSnapshot(`
+    [
+      {
+        "category": "기타",
+        "date": "2024-10-15",
+        "description": "삭제할 이벤트입니다",
+        "endTime": "10:00",
+        "id": "1",
+        "location": "어딘가",
+        "notificationTime": 10,
+        "repeat": {
+          "interval": 0,
+          "type": "none",
+        },
+        "startTime": "09:00",
+        "title": "삭제할 이벤트",
+      },
+    ]
+  `);
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 1000,
+    status: 'info',
+    title: '일정 로딩 완료!',
+  });
+
+  expect(toastFn).toHaveBeenCalledWith({
+    duration: 3000,
+    isClosable: true,
+    status: 'error',
+    title: '일정 삭제 실패',
+  });
+});
--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -1,14 +1,81 @@
 import { act, renderHook } from '@testing-library/react';
+import { vi } from 'vitest';
 
+import { events } from '../../__mocks__/response/realEvents.json';
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
-import { parseHM } from '../utils.ts';
 
-it('초기 상태에서는 알림이 없어야 한다', () => {});
+it('초기 상태에서는 알림이 없어야 한다', () => {
+  const { result } = renderHook(() => useNotifications(events as Event[]));
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+  expect(result.current.notifications).toMatchInlineSnapshot(`[]`);
+});
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {
+  vi.setSystemTime(new Date('2025-02-20T09:59:00'));
+  const { result } = renderHook(() => useNotifications(events as Event[]));
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  act(() => vi.advanceTimersByTime(1000));
+
+  expect(result.current.notifications).toMatchInlineSnapshot(`
+    [
+      {
+        "id": "2b7545a6-ebee-426c-b906-2329bc8d62bd",
+        "message": "1분 후 팀 회의 일정이 시작됩니다.",
+      },
+    ]
+  `);
+});
+
+it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {
+  vi.setSystemTime(new Date('2025-02-20T09:59:00'));
+  const { result } = renderHook(() => useNotifications(events as Event[]));
+  act(() => vi.advanceTimersByTime(1000));
+
+  expect(result.current.notifications).toMatchInlineSnapshot(`
+    [
+      {
+        "id": "2b7545a6-ebee-426c-b906-2329bc8d62bd",
+        "message": "1분 후 팀 회의 일정이 시작됩니다.",
+      },
+    ]
+  `);
+
+  act(() => result.current.removeNotification(0));
+  expect(result.current.notifications).toEqual([]);
+});
+
+it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {
+  vi.setSystemTime(new Date('2025-02-20T09:59:00'));
+  const { result } = renderHook(() => useNotifications(events as Event[]));
+  act(() => vi.advanceTimersByTime(1000));
+
+  expect(result.current.notifiedEvents).toMatchInlineSnapshot(`
+    [
+      "2b7545a6-ebee-426c-b906-2329bc8d62bd",
+    ]
+  `);
+  expect(result.current.notifications).toMatchInlineSnapshot(`
+    [
+      {
+        "id": "2b7545a6-ebee-426c-b906-2329bc8d62bd",
+        "message": "1분 후 팀 회의 일정이 시작됩니다.",
+      },
+    ]
+  `);
+
+  act(() => vi.advanceTimersByTime(1000));
+  expect(result.current.notifiedEvents).toMatchInlineSnapshot(`
+    [
+      "2b7545a6-ebee-426c-b906-2329bc8d62bd",
+    ]
+  `);
+  expect(result.current.notifications).toMatchInlineSnapshot(`
+    [
+      {
+        "id": "2b7545a6-ebee-426c-b906-2329bc8d62bd",
+        "message": "1분 후 팀 회의 일정이 시작됩니다.",
+      },
+    ]
+  `);
+});
